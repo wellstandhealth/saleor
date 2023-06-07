8b584f020703944d9ca3b8c3104ee9c044024c22
jordanemt-wellstand <jordan.mendez@wellstandhealth.com>
2023-06-07 14:41:09 -0600

    Add ExternalUserMapping model

--- saleor/pharmacy/models.py
+++ saleor/pharmacy/models.py
@@ -1,5 +1,26 @@
+import uuid
 from django.db import models
-from django.conf import settings
-from uuid import uuid4
 
-from . import Gender
+from saleor.account.models import User
+
+
+class BaseAuditMixin(models.Model):
+    id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True,
+        db_column="uuid",
+        unique=True,
+    )
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class ExternalUserMapping(BaseAuditMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    external_id = models.CharField(max_length=255, db_index=True)
+    external_system = models.CharField(max_length=25)
