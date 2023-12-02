ee37ef725ea4b8af646b5115c6ea101bc079b23e
Paul Tindall <paul@wellstandhealth.com>
2023-12-02 12:29:27 -0600

    WSP-438 fix celery.conf

--- saleor/celeryconf.py
+++ saleor/celeryconf.py
@@ -33,11 +33,5 @@ app.autodiscover_tasks(
     ],
     related_name="saleor3_14",
 )
-app.autodiscover_tasks(
-    packages=[
-        "saleor.discount.migrations.tasks",
-    ],
-    related_name="saleor3_16",
-)
 app.autodiscover_tasks(lambda: discover_plugins_modules(settings.PLUGINS))  # type: ignore[misc] # circular import # noqa: E501
 app.autodiscover_tasks(related_name="search_tasks")
